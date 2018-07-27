913ba95ec936beee55ef7ddeb8f45155ab6dae86
tmslav <tmslav@gmail.com>
2018-07-27 15:52:55 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {1..10}
   do
-    curl --data '{"username":"tmslav@gmail.com","password":"Laajvi123","site_name":"ah.nl","product_id":"wi737"}' localhost:8000/shop-api/
+    curl --data '{"username":"tmslav@gmail.com","password":"","site_name":"ah.nl","product_id":"wi737"}' localhost:8000/shop-api/
     printf "\n"
 done
